629b1de4b06a20346278d43b98c9aae6464536d3
gitdevstar <loganph.work@gmail.com>
2023-07-10 10:01:58 -0400

    enable to input url itself (0.2)

--- app/src/main/java/com/devstar/deeplinkwebview/MainActivity.kt
+++ app/src/main/java/com/devstar/deeplinkwebview/MainActivity.kt
@@ -1,94 +1,82 @@
 package com.devstar.deeplinkwebview
 
-import android.app.PendingIntent.getActivity
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.view.ViewGroup
-import android.webkit.WebSettings.LOAD_NO_CACHE
-import android.webkit.WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.inputmethod.EditorInfo
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat.startActivity
-import com.devstar.deeplinkwebview.ui.theme.DeeplinkWebViewTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import com.devstar.deeplinkwebview.databinding.ActivityMainBinding
 
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DeeplinkWebViewTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    FUllWebView()
-                }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        webView = binding.webview
+        webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                binding.progressCircular.visibility = VISIBLE
+                binding.inputUrl.setText(url)
             }
-        }
-    }
-}
 
-@Composable
-fun FUllWebView() {
-    // Declare a string that contains a url
-    // Adding a WebView inside AndroidView
-    // with layout as full screen
-    AndroidView(factory = {
-        WebView(it).apply {
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-            )
-            webViewClient = WebViewClient()
-            settings.loadsImagesAutomatically = true
-            settings.javaScriptEnabled = true
-            settings.cacheMode = LOAD_NO_CACHE
-            settings.builtInZoomControls = true
-            settings.displayZoomControls = true
-            settings.domStorageEnabled = true
-            settings.mixedContentMode = MIXED_CONTENT_COMPATIBILITY_MODE
-            loadUrl(Constants.WEBVIEW_LOAD_URL)
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
 
-            webViewClient = object : WebViewClient() {
-                override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                    if (url.contains(Constants.DEEP_LINK_PREFIX)) {
-                        Log.d("MainActivity", "shouldOverrideUrlLoading: $url")
-                        try {
-                            val intent = Intent(Intent.ACTION_VIEW)
-                            intent.data = Uri.parse(url)
-                            startActivity(context, intent, null)
-                        } catch (e: Exception) {
-                            val intent = Intent(Intent.ACTION_VIEW)
-                            intent.data = Uri.parse(Constants.PAYTM_URL)
-                            startActivity(context, intent, null)
-                        }
-
-                    } else {
-                        view?.loadUrl(url)
+                if (url.contains(Constants.DEEP_LINK_PREFIX)) {
+                    Log.d("MainActivity", "shouldOverrideUrlLoading: $url")
+                    try {
+                        val intent = Intent(Intent.ACTION_VIEW)
+                        intent.data = Uri.parse(url)
+                        startActivity(intent, null)
+                    } catch (e: Exception) {
+                        val intent = Intent(Intent.ACTION_VIEW)
+                        intent.data = Uri.parse(Constants.PAYTM_URL)
+                        startActivity(intent, null)
                     }
-                    return true
+
+                } else {
+                    view?.loadUrl(url)
+
                 }
+                return true
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                // TODO Auto-generated method stub
+                super.onPageFinished(view, url)
+                binding.progressCircular.visibility = GONE
             }
         }
-    }, update = {
-        it.loadUrl(Constants.WEBVIEW_LOAD_URL)
-    })
-}
 
-@Preview(showBackground = true)
-@Composable
-fun FUllWebViewPreview() {
-    DeeplinkWebViewTheme {
-        FUllWebView()
+        // this will load the url of the website
+        webView.loadUrl(Constants.WEBVIEW_LOAD_URL)
+
+        binding.inputUrl.setOnEditorActionListener { inputView , actionId, event ->
+            if(actionId == EditorInfo.IME_ACTION_GO){
+                webView.loadUrl(inputView.text.toString())
+                true
+            } else false
+        }
+        binding.btnBack.setOnClickListener {
+            webView.goBack()
+        }
+        binding.btnForward.setOnClickListener {
+            webView.goForward()
+        }
+
     }
 }
